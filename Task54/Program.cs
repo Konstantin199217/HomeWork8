a02d7e4c6cf0d96f33ba28a60faa32d0651b5ebe
Konstantin <kot199217@mail.ru>
2022-10-13 23:57:45 +0900

    end Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+Console.Write("Введите количество строк 1 массива: ");
+int rowsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов 1 массива: ");
+int columnsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк 2 массива: ");
+int rowsB = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов 2 массива: ");
+int columnsB = int.Parse(Console.ReadLine());
+if (columnsA != rowsB)
+{
+    Console.WriteLine("Такие матрицы умножать нельзя!");
+    return;
+}
+
+int[,] CreateArray(int r, int c)
+{
+    int[,] Array = new int[r, c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            Array[i, j] = new Random().Next(1, 9);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FirstMatrix = CreateArray(rowsA, columnsA);
+int[,] SecondMatrix = CreateArray(rowsB, columnsB);
+
+int[,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
+
+Console.WriteLine("Первая матрица ");
+PrintArray(FirstMatrix);
+Console.WriteLine();
+Console.WriteLine("Вторая матрица ");
+PrintArray(SecondMatrix);
+Console.WriteLine();
+PrintArray(GetMultiplicationMatrix(FirstMatrix, SecondMatrix));
